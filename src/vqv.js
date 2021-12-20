9f3293e265d15876a1aaf711cbf96d93f7b006d7
Suelen Arruda <suelenonyrius@gmail.com>
2021-12-20 19:24:48 +0100

    requisito 3, nao acredito que estou voltando ao ritmo uhuuu

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if ((nome === null && idade === null) || (nome === undefined && idade === undefined)) {
+    return undefined;
+  }
+  const apresentacao = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return apresentacao;
+};
 
 module.exports = vqv;
